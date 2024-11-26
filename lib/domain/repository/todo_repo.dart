acd0fb7156885201fe1d643f724c50115c863f0a
arrohisrivastava <arrohisrivastava6@gmail.com>
2024-11-26 23:56:06 +0530

    Add Todo modal, repo, and DB structure

---
+++ lib/domain/repository/todo_repo.dart
@@ -0,0 +1,9 @@
+//Interface
+
+import 'package:todo/domain/models/todo.dart';
+abstract class TodoRepo{
+  Future<List<Todo>> getTodos();
+  Future<void> addTodo(Todo todo);
+  Future<void> deleteTodo(Todo todo);
+  Future<void> updateTodo(Todo todo);
+}
